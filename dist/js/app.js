5c7f11392d348dedde6fe0f1aa2c633b2b98d63d
Daniel Costa <dancosta.id@gmail.com>
2022-10-15 15:04:52 -0300

    Just saving progress while I keep studying. Nothing major.

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,9 @@
+import { NegociacaoController } from "./controllers/negociacaoController.js";
 import { Negociacao } from "./models/negociacao.js";
+const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 10, 25);
-console.log('negociacao', negociacao.volume);
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    controller.adiciona();
+});

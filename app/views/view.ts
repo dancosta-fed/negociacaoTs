74c272f76a7d6aefed19314d7bc6cd0dbe7faa81
Daniel Costa <dancosta.id@gmail.com>
2022-10-16 19:52:14 -0300

    saving progress

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,14 +1,23 @@
 // <T> == is for type. It will follow the child's.
 export abstract class View<T> { 
-  protected elemento: HTMLElement;
   // use PROTECTED instead of PRIVATE because it will only allow the child to acess the element.
+  protected elemento: HTMLElement;
+  private escape = false;
 
-  constructor(selector: string) {
+  constructor(selector: string, escape?: boolean) {
     this.elemento = document.querySelector(selector);
+
+    if (escape) {
+      this.escape = escape;
+    }
   }
   // using public isn't necessary. When there's nothings.. you assume the method is public.
   public update(model: T): void {
-    const template = this.template(model);
+    let template = this.template(model);
+    if (this.escape) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+      // Remove any script could  be insert in the code.
+    }
     this.elemento.innerHTML = template;
   }
   
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,12 +1,19 @@
 // <T> == is for type. It will follow the child's.
 export class View {
-    // use PROTECTED instead of PRIVATE because it will only allow the child to acess the element.
-    constructor(selector) {
+    constructor(selector, escape) {
+        this.escape = false;
         this.elemento = document.querySelector(selector);
+        if (escape) {
+            this.escape = escape;
+        }
     }
     // using public isn't necessary. When there's nothings.. you assume the method is public.
     update(model) {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escape) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+            // Remove any script could  be insert in the code.
+        }
         this.elemento.innerHTML = template;
     }
 }
